d84fcf20f3982a8c0c5dd16eb59aadf9fd22675a
Ramsey McGrath <ramseymcgrath@gmail.com>
2025-08-16 01:30:44 -0400

    bump vers
    Signed-off-by: Ramsey McGrath <ramseymcgrath@gmail.com>

--- src/__version__.py
+++ src/__version__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Version information for PCILeech Firmware Generator."""
 
-__version__ = "0.13.1"
-__version_info__ = (0, 13, 1)
+__version__ = "0.13.2"
+__version_info__ = (0, 13, 2)
 
 # Release information
 __title__ = "PCILeech Firmware Generator"
@@ -13,5 +13,5 @@ __license__ = "MIT"
 __url__ = "https://github.com/ramseymcgrath/PCILeechFWGenerator"
 
 # Build metadata
-__build_date__ = "2025-08-16T03:05:13.065477"
-__commit_hash__ = "fe0cafc"
+__build_date__ = "2025-08-16T05:30:26.537947"
+__commit_hash__ = "729b91d"
